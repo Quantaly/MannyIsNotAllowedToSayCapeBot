8b52e2d3845393bd4284633ddee180fcecdfc98b
quantumAssembly <kaibug@gmail.com>
2017-10-11 15:51:51 -0500

    Refactored cape detection
    Not much point in having it in separate files right now. Maybe in future.

--- tools/inspectMessage.js
+++ tools/inspectMessage.js
@@ -1,13 +1,4 @@
 info = require("./cape.json");
 
-function regexpFrom(input) {
-	return new RegExp(input.source, input.flags);
-}
-blacklist = info.blacklist.map(regexpFrom);
-whitelist = info.whitelist.map(regexpFrom);
-
-module.exports = function(message) {
-	for (i in whitelist) if (whitelist[i].test(message)) return false;
-	for (i in blacklist) if (blacklist[i].test(message)) return true;
-	return false;
-}
+blacklist = new RegExp("\\b[ck]\\s*[a@]+\\s*p\\s*e", "i");
+module.exports = (message) => blacklist.test(message);
